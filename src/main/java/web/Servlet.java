ae184f51cbe3bffae8d02be38f4ba10404da1528
Hugo Maldonado <Hugo Maldonado>
2021-11-12 15:57:20 -0300

    Manejo de estados HTTP

--- src/main/java/web/Servlet.java
+++ src/main/java/web/Servlet.java
@@ -3,7 +3,6 @@ package web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
